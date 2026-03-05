19dd62da79fa45ce4326053e801eb0d9a683674e
davidmariusz06-afk <davidmariusz06@gmail.com>
2026-03-05 01:46:23 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // The Prophecy — Service Worker
-const CACHE_VERSION = 'prophecy-v19';
+const CACHE_VERSION = 'prophecy-v20';
 const STATIC_ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
